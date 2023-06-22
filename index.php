b5d915b3218353d2eaa5514bd1f01aa2d30b7a02
valeria caramellino <valeriacaramellino@gmail.com>
2023-06-22 15:33:55 +0200

    feat

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-<?php 
- ini_set('display_errors', 1);
- ini_set('display_startup_errors', 1);
- error_reporting(E_ALL);
+<?php
+//per vedere errori in pagina 
+ //ini_set('display_errors', 1);
+ //ini_set('display_startup_errors', 1);
+ //error_reporting(E_ALL);
 
 
 require __DIR__ . "/models/Categoria.php";
